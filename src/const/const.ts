2342958e73fff58f273132d94ca6c370af757520
Anni-spb <uventa2@yandex.ru>
2024-02-27 13:55:56 +0300

    fix enum CITIES

--- src/const/const.ts
+++ src/const/const.ts
@@ -2,7 +2,7 @@ export const Setting = {
   ResultCount: 312,
 } as const;
 
-export const CITIES: string[] = ['Paris', 'Cologne', 'Brussels', 'Amsterdam', 'Hamburg', 'Dusseldorf'] as const;
+export const CITIES = ['Paris', 'Cologne', 'Brussels', 'Amsterdam', 'Hamburg', 'Dusseldorf'] as const;
 
 export enum AppRoute {
   Root = '/',
